2329fc467db72f4922ddba2d6d2042e533d19f81
PedroDominguez <53623989+PedroDominguezUTEC@users.noreply.github.com>
2022-06-07 21:49:05 -0500

    Update apifactorial.py
    fibo added

--- apifactorial.py
+++ apifactorial.py
@@ -10,6 +10,15 @@ def factorial(n):
     result = math.factorial(n)
     return "El resultado es " + str(result)
 
+@app.route('/fibonacci/<n>')
+def fibonacci(n):
+    #return str(round(1.61803**(int(n)) / 5**0.5))
+    n = int(n)
+    lis=[1, 1]
+    for i in range(n-2):
+        lis.append(lis[-1] + lis[-2])
+    return str(lis[-1])
+
 
 
 app.run(port=5002)
